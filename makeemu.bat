ea4a39be52996f2b32b24ed33f4103d1a88281b0
Andrey Kutejko <andy128k@gmail.com>
2010-01-12 20:30:57 +0200

    initial commit

---
+++ makeemu.bat
@@ -0,0 +1,28 @@
+if not %POCKETBOOKSDK%.==. goto C1
+echo Environment variable POCKETBOOKSDK is not set
+pause
+:C1
+set PATH=%POCKETBOOKSDK%\bin;%PATH%
+
+set INCLUDE=
+set LIBS=-linkview -lfreetype -ljpeg -lz -lgdi32
+set OUTPUT=pb-mahjong.exe
+
+rm -f %OUTPUT%
+
+set IMAGES=
+if not exist images\*.bmp goto NOIMG
+set IMAGES=%TEMP%\images.temp.c
+pbres -c %IMAGES% images/*.bmp
+if errorlevel 1 goto L_ER
+:NOIMG
+
+gcc -static -Wall -O2 -fomit-frame-pointer %INCLUDE% -o %OUTPUT% src/*.c %IMAGES% %LIBS%
+if errorlevel 1 goto L_ER
+
+pause
+exit 0
+
+:L_ER
+pause
+exit 1
